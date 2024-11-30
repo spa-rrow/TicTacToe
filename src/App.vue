89cb9a925d080bf00f373cc55fa26a76aee59ff0
Sparrow <otieno.francis@strathmore.edu>
2024-11-30 13:25:01 +0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main class="pt-8 text-center dark:bg-gray-800 min-h-screen dark:text-white">
+    <h1 class="mb-8 text-3xl font-bold uppercase">Tic Tac Toe</h1>
+
+    <h3 class="text-xl mb-4">Player {{player}}'s turn</h3>
+
+    <div class="flex flex-col items-center mb-8">
+      <div
+        v-for="(row, x) in board"
+        :key="x"
+        class="flex">
+        <div
+            v-for="(cell,y) in row"
+            :key="y"
+            @click="MakeMove(x,y)"
+            :class="`border border-white w-20 h-20 hover:bg-gray-700 flex items-center justify-center material-icons-outlined text-4xl cursor-pointer ${cell === 'x' ? 'text-pink-500' : 'text-blue-500'}`"
+        >{{cell}}</div>
+      </div>
+    </div>
+
+    <h2 class="text-6xl fond-bold mb-8">Player '{{winner}}' wins!</h2>
+
+    <button @click = "ResetGame" class="px-4 py-2 bg-pink-500 rounded uppercase font-bold hover:bg-pink-600 duration-300">Reset Game</button>
+  </main>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import {ref, computed} from 'vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+const player = ref('x');
+const board = ref([
+    ['','',''],
+    ['','',''],
+    ['','',''],
+])
+
+const CalculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
   }
+  return null;
+}
+
+const winner = computed(() => CalculateWinner(board.value.flat()));
+
+const MakeMove = (x,y) => {
+  if(winner.value)return
+  if(board.value[x][y]) return
+
+  board.value[x][y] = player.value
+
+  player.value = player.value === 'x' ? 'o' : 'x';
+
+}
+
+const ResetGame = () => {
+  board.value = [
+    ['','',''],
+    ['','',''],
+    ['','',''],
+  ]
+  player.value = 'x'
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
